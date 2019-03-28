a14a6878b281c8d72928c68ce5a468fde4454436
Kunal <patelkr07@gmail.com>
2019-03-28 12:08:50 -0500

    Initial setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    $("button").click(function() {
+        
+    });
+
+}); 
+
+const questions = [
+    {
+        question: "Example question 1", 
+
+        choices: {
+            a: "example answer 1", b: "example answer 2", c: "example answer 3", d: "example answer 4"
+        },
+
+        answer: "b"
+
+    },
+
+];
+
+console.log(questions); 
+
+    
