5ae5c6d056bba1ff17e79695b1ff45fbb1016cf2
Kunal <patelkr07@gmail.com>
2019-03-28 12:29:52 -0500

    Array placeholder

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,8 +18,111 @@ const questions = [
 
     },
 
+    {
+        question: "Example question 2", 
+
+        choices: {
+            a: "example answer 1", b: "example answer 2", c: "example answer 3", d: "example answer 4"
+        },
+
+        answer: "b"
+
+    },
+
+    {
+        question: "Example question 3", 
+
+        choices: {
+            a: "example answer 1", b: "example answer 2", c: "example answer 3", d: "example answer 4"
+        },
+
+        answer: "b"
+
+    },
+
+    {
+        question: "Example question 4", 
+
+        choices: {
+            a: "example answer 1", b: "example answer 2", c: "example answer 3", d: "example answer 4"
+        },
+
+        answer: "b"
+
+    },
+
+    {
+        question: "Example question 5", 
+
+        choices: {
+            a: "example answer 1", b: "example answer 2", c: "example answer 3", d: "example answer 4"
+        },
+
+        answer: "b"
+
+    },
+
+    {
+        question: "Example question 6", 
+
+        choices: {
+            a: "example answer 1", b: "example answer 2", c: "example answer 3", d: "example answer 4"
+        },
+
+        answer: "b"
+
+    },
+
+    {
+        question: "Example question 7", 
+
+        choices: {
+            a: "example answer 1", b: "example answer 2", c: "example answer 3", d: "example answer 4"
+        },
+
+        answer: "b"
+
+    },
+
+    {
+        question: "Example question 8", 
+
+        choices: {
+            a: "example answer 1", b: "example answer 2", c: "example answer 3", d: "example answer 4"
+        },
+
+        answer: "b"
+
+    },
+
+    {
+        question: "Example question 9", 
+
+        choices: {
+            a: "example answer 1", b: "example answer 2", c: "example answer 3", d: "example answer 4"
+        },
+
+        answer: "b"
+
+    },
+
+    {
+        question: "Example question 10", 
+
+        choices: {
+            a: "example answer 1", b: "example answer 2", c: "example answer 3", d: "example answer 4"
+        },
+
+        answer: "b"
+
+    },
+
+
+
+
+
+
 ];
 
-console.log(questions); 
+console.log(questions[5]); 
 
-    
